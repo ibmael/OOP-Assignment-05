2ebbd5698ae0ef7202b8bbce6cc171b1f5f6c07a
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-08-22 11:50:41 +0300

    Part 02 Q8 & Q9 - Add shipment extension methods

---
+++ TASK9ROUTE/Core/Shipment.Tracking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TASK9ROUTE
+{
+    internal abstract partial class Shipment
+    {
+        #region Tracking
+
+        public string TrackingStatus { get; set; } = "Ready";
+
+        public string GetTrackingStatus()
+        {
+            return TrackingStatus;
+        }
+
+        public void UpdateTrackingStatus(string newStatus)
+        {
+            TrackingStatus = newStatus;
+        }
+
+        #endregion
+    }
+}
